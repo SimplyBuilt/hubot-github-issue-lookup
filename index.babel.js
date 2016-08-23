08f0f199ce35f3f0360564b3e11298204f0f9286
Michael Coyne <mikeycgto@gmail.com>
2016-08-23 10:39:06 -0400

    Use more es6

--- index.babel.js
+++ index.babel.js
@@ -9,30 +9,32 @@
 // Commands:
 //   #nnn - link to Github issue nn from HUBOT_GITHUB_REPO project
 //
-// Notes:
-//   If, for example, HUBOT_GITHUB_USER_JOHN is set to GitHub user login
-//   'johndoe1', you can ask `show john's issues` instead of `show johndoe1's
-//   issues`. This is useful for mapping chat handles to GitHub logins.
+// Dependencies:
+//   "githubot": "1.0.0"
 //
-//   HUBOT_GITHUB_API allows you to set a custom URL path (for Github enterprise users)
+// Notes:
+//   The HUBOT_GITHUB_TOKEN and HUBOT_GITHUB_REPO env vars are required.
 //
 // Author:
 //   Michael Coyne (@mikeycgto)
 
+import {default as GitHubot} from 'githubot';
+
 const TOKEN = process.env.HUBOT_GITHUB_TOKEN;
 const REPO  = process.env.HUBOT_GITHUB_REPO;
+
 const IGNORED = (process.env.HUBOT_GITHUB_ISSUE_LINK_IGNORE_USERS || '').split(',');
 
-module.exports = (robot) => {
+export default (robot) => {
     if (TOKEN == undefined || REPO == undefined){
         console.warn("Missing HUBOT_GITHUB_TOKEN and/or HUBOT_GITHUB_REPO");
         return;
     }
 
-    const github = require("githubot")(robot);
+    const github = GitHubot(robot);
 
     robot.hear(/((\S*|^)?#(\d+)).*/, (msg) => {
-        var issue = +msg.match[3];
+        const issue = +msg.match[3];
 
         if (issue == undefined || isNaN(issue))
             return;
@@ -41,7 +43,7 @@ module.exports = (robot) => {
             return;
 
         github.get(`repos/${REPO}/issues/${issue}`, (issue_resp) => {
-            var state = issue_resp.state || 'UNKNOWN';
+            const state = issue_resp.state || 'UNKNOWN';
 
             msg.send(`Issue ${issue}: ${issue_resp.title} (${state.toUpperCase()} - ${issue_resp.html_url})`);
         });
